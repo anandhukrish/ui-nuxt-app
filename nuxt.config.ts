cf4dc9544addf1599a798a3129b87ff2b927d957
Ananthu Krishnan <ananthukrish.k@gmail.com>
2025-05-31 16:22:03 +0530

    testing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,28 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-import Aura from "@primeuix/themes/aura";
+// import Aura from "@primeuix/themes/aura";
 
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
 
   modules: [
-    "@primevue/nuxt-module",
+    // "@primevue/nuxt-module",
     "@nuxt/icon",
     "@pinia/nuxt",
     "@nuxt/image",
     "@nuxt/test-utils",
   ],
 
-  primevue: {
-    options: {
-      theme: {
-        preset: Aura,
-        options: {
-          darkModeSelector: false,
-        },
-      },
-    },
-  },
+  // primevue: {
+  //   options: {
+  //     theme: {
+  //       preset: Aura,
+  //       options: {
+  //         darkModeSelector: false,
+  //       },
+  //     },
+  //   },
+  // },
   ssr: false,
 });
